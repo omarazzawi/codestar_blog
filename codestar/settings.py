c4a0bcfb34d86fb278e98c8bb141b591ccb63788
omarazzawi <omar.alazzawi1987@gmail.com>
2025-10-23 15:59:20 +0200

    PostgreSQL database configuration

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,6 @@ SECRET_KEY = 'django-insecure-tfzmn+j%(51nu-$m$9pifs0513jq&pmm^qv59#^qsq_a)a@+5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', ]
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', ]
 
 
@@ -75,11 +78,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+     "postgresql://neondb_owner:npg_Z9JbxRBGENS3@ep-late-cherry-agymxr9n.c-2.eu-central-1.aws.neon.tech/sauna_keep_wager_679117")
