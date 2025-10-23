73c42434bd4bd60a31471e1bd964fae634b8c190
omarazzawi <omar.alazzawi1987@gmail.com>
2025-10-23 11:48:15 +0200

    configure Gunicorn as production server

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tfzmn+j%(51nu-$m$9pifs0513jq&pmm^qv59#^qsq_a)a@+5b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
