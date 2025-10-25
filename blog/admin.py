7c9d8def7178f5ec16233d5843de404a7a7bf7b0
omarazzawi <omar.alazzawi1987@gmail.com>
2025-10-25 13:01:59 +0200

    Add Post model for blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+#  This will allow you to create, update and delete blog posts from the admin panel
+admin.site.register(Post)
