d1501dbce19d92e4a61dee6078a6704e085108f3
omarazzawi <omar.alazzawi1987@gmail.com>
2025-10-28 14:22:24 +0100

    Adds a fixtures directory and a posts.json

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"The title of this post is {self.title}"
