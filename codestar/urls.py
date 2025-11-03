f45da4848e2e2047c47196d7ec22d1101e3de1c3
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-03 21:58:28 +0100

    Add a personalized message with user name

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("about/", include("about.urls"), name="about-urls"),
+    path("about/", include("about.urls")),
     path("accounts/", include("allauth.urls")),
-    path("", include("blog.urls"), name="blog-urls"),
-    path('summernote/', include('django_summernote.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
+    path("summernote/", include("django_summernote.urls")),
+    path("admin/", admin.site.urls),
 ]
