b54d2c497d8a30db42c8704975d25d5e612b3a6a
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-04 01:03:49 +0100

    Add form submission with POST handling and database persistence

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("about/", include("about.urls")),
     path("accounts/", include("allauth.urls")),
     path("", include("blog.urls")),
     path("summernote/", include("django_summernote.urls")),
-    path("admin/", admin.site.urls),
+    
 ]
