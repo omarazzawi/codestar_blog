e9743caafe84dafaac51e4a8b4c1a200f44b13e1
omarazzawi <omar.alazzawi1987@gmail.com>
2025-11-07 14:22:01 +0100

    enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
@@ -8,6 +10,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
+    
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
--- env.py
+++ env.py
@@ -5,3 +5,6 @@ os.environ.setdefault(
      "postgresql://neondb_owner:npg_Z9JbxRBGENS3@ep-late-cherry-agymxr9n.c-2.eu-central-1.aws.neon.tech/sauna_keep_wager_679117")
 
 os.environ.setdefault("SECRET_KEY", "q%n_xl=utfoyzg78k_03t0_f4fe8^a+)ycw+z5d1a!*uuhtn5^")
+
+os.environ.setdefault(
+        "CLOUDINARY_URL", "cloudinary://812589721343249:7GpgTm_yeVXBC9CWbhWlt8e02qo@dfqmgd0el")
